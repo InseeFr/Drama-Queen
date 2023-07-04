4c6c3d21bcf26ea1aa6e5a7463c86d851bfdba8a
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2023-07-04 16:55:33 +0200

    Update SurveyUnitMapping.tsx

--- drama-queen/src/pages/queenMapping/SurveyUnitMapping.tsx
+++ drama-queen/src/pages/queenMapping/SurveyUnitMapping.tsx
@@ -1,5 +1,5 @@
 import { db } from 'indexedDb'
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { useLiveQuery } from "dexie-react-hooks";
 import { READ_ONLY } from "utils/constants";
 
@@ -17,5 +17,9 @@ export function SurveyUnitMapping() {
 
   if (!surveyUnit) return <div>In Progress</div>
 
-  return <Navigate to={(`/queen/${readonly ? `${readonly}/` : ''}questionnaire/${surveyUnit?.questionnaireId}/survey-unit/${id}`)} />
+  //return <Navigate to={(`/queen/${readonly ? `${readonly}/` : ''}questionnaire/${surveyUnit?.questionnaireId}/survey-unit/${id}`)} />
+  //We know that we will reload the entire app but it's due on purpose
+  //Because of a faulty architecture and a queen non-modification constraint, we need to know which queen we're going to redirect to when the DramaQueen is mounted.
+  window.location.href = `/queen/${readonly ? `${readonly}/` : ''}questionnaire/${surveyUnit?.questionnaireId}/survey-unit/${id}`;
+  return null;
 }
