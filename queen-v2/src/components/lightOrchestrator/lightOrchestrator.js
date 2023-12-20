bbe49e35dffa76436054b4528015b4ce6df467fa
Laurent Caouissin <caouissin.laurent@gmail.com>
2023-12-20 15:52:14 +0100

    Fix #95

--- queen-v2/src/components/lightOrchestrator/lightOrchestrator.js
+++ queen-v2/src/components/lightOrchestrator/lightOrchestrator.js
@@ -1,16 +1,16 @@
 import { useLunatic } from '@inseefr/lunatic';
-import { memo, useEffect, useMemo, useRef } from 'react';
-import ButtonContinue from './buttons/continue/index';
 import D from 'i18n';
+import { memo, useEffect, useMemo, useRef } from 'react';
+import { Link } from 'react-router-dom';
 import { isSequenceOrSubsequenceComponent } from 'utils/components/deduceState';
 import { QUEEN_URL } from 'utils/constants';
 import { useConstCallback } from 'utils/hook/useConstCallback';
 import { LoopPanel } from './LoopPanel';
+import ButtonContinue from './buttons/continue/index';
 import { ComponentDisplayer } from './componentDisplayer';
 import Header from './header';
 import { useStyles } from './lightOrchestrator.style';
 import NavBar from './navBar';
-import { Link } from 'react-router-dom';
 
 function noDataChange() {
   /**/
@@ -191,7 +191,8 @@ function LightOrchestrator({
         readonly={readonly}
         quit={memoQuit}
         definitiveQuit={memoDefinitiveQuit}
-        currentPage={page}
+        pageTag={pageTag}
+        pager={pager}
       />
       <div className={classes.bodyContainer}>
         <div className={classes.mainTile}>
