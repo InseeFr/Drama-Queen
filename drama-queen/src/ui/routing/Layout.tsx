47c1e8bc2ae93607c06aba619df97bf70022c953
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-01-22 11:49:52 +0100

    add global style

--- drama-queen/src/ui/routing/Layout.tsx
+++ drama-queen/src/ui/routing/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import { AppVersion } from 'ui/components/appVersion'
+import { globalStyles } from 'ui/style/globalStyles'
 
 export function Layout() {
   return (
     <>
+      {globalStyles}
       <Outlet />
-      <AppVersion />
+      {/* <AppVersion /> */}
     </>
   )
 }
---
+++ drama-queen/src/ui/style/globalStyles.tsx
@@ -0,0 +1,16 @@
+import { GlobalStyles } from 'tss-react'
+
+export const globalStyles = (
+  <GlobalStyles
+    styles={{
+      html: {
+        height: '100%',
+        margin: 0,
+      },
+      body: {
+        height: '100%',
+        margin: 0,
+      },
+    }}
+  />
+)
