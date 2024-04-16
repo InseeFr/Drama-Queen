9c9a3acf8d4d95726e267aa9f0d9d49fff0e47a9
Dylan Decrulle <81740200+ddecrulle@users.noreply.github.com>
2024-04-16 16:16:05 +0200

    add vite-env, make docker image root less
    helm chart needs to be adapted (service port 80 -> 8080)

--- drama-queen/vite.config.ts
+++ drama-queen/vite.config.ts
@@ -3,78 +3,79 @@ import federation from '@originjs/vite-plugin-federation'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
+import { viteEnvs } from 'vite-envs'
 
 // https://vitejs.dev/config/
-export default defineConfig(({}) => {
-  return {
-    define: {
-      LUNATIC_VERSION: JSON.stringify(
-        process.env.npm_package_dependencies__inseefr_lunatic
-      ),
-      APP_VERSION: JSON.stringify(process.env.npm_package_version),
-    },
-    plugins: [
-      react(),
-      federation({
-        name: 'drama-queen',
-        // remotes: {
-        // //This is the right way of mfe with vite module federation (but legacy queens does not use)
-        // queen: {
-        //   external: `Promise.resolve((window?._env_?.["VITE_QUEEN_URL"] || import.meta.env["VITE_QUEEN_URL"]) + "/entry.js")`,
-        //   externalType: "promise",
-        // },
-        // },
-        filename: 'remoteEntry.js',
-        exposes: {
-          './DramaIndex': './src/bootstrap.tsx',
-        },
-        shared: ['react', 'react-dom', 'react-router-dom'],
+export default defineConfig({
+  define: {
+    LUNATIC_VERSION: JSON.stringify(
+      process.env.npm_package_dependencies__inseefr_lunatic
+    ),
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+  },
+  plugins: [
+    react(),
+    federation({
+      name: 'drama-queen',
+      // remotes: {
+      // //This is the right way of mfe with vite module federation (but legacy queens does not use)
+      // queen: {
+      //   external: `Promise.resolve((window?._env_?.["VITE_QUEEN_URL"] || import.meta.env["VITE_QUEEN_URL"]) + "/entry.js")`,
+      //   externalType: "promise",
+      // },
+      // },
+      filename: 'remoteEntry.js',
+      exposes: {
+        './DramaIndex': './src/bootstrap.tsx',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+    tsconfigPaths(),
+    viteEnvs({
+      computedEnv: () => ({
+        APP_VERSION: process.env.npm_package_version,
       }),
-      tsconfigPaths(),
-      VitePWA({
-        //Generate the external service worker for pearl
-        injectRegister: false,
-        strategies: 'injectManifest',
-        manifest: false,
-        srcDir: 'src',
-        filename: 'queen-service-worker.js',
-      }),
-      VitePWA({
-        injectRegister: 'auto',
-        strategies: 'injectManifest',
-        srcDir: 'src',
-        filename: 'sw.js',
-        includeAssets: [
-          'favicon.ico',
-          'apple-touch-icon.png',
-          'masked-icon.svg',
+      injectWorkers: ['queen-service-worker.js'],
+    }),
+    VitePWA({
+      //Generate the external service worker for pearl
+      injectRegister: false,
+      strategies: 'injectManifest',
+      manifest: false,
+      srcDir: 'src',
+      filename: 'queen-service-worker.js',
+    }),
+    VitePWA({
+      injectRegister: 'auto',
+      strategies: 'injectManifest',
+      srcDir: 'src',
+      filename: 'sw.js',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Questionnaire',
+        short_name: 'Questionnaire',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
         ],
-        manifest: {
-          name: 'Questionnaire',
-          short_name: 'Questionnaire',
-          theme_color: '#ffffff',
-          background_color: '#ffffff',
-          display: 'standalone',
-          orientation: 'portrait',
-          icons: [
-            {
-              src: 'android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'android-chrome-256x256.png',
-              sizes: '256x256',
-              type: 'image/png',
-            },
-          ],
-        },
-      }),
-    ],
-    build: {
-      target: 'esnext',
-      minify: true,
-      sourcemap: true,
-    },
-  }
+      },
+    }),
+  ],
+  build: {
+    target: 'esnext',
+    minify: true,
+    sourcemap: true,
+  },
 })
