2bcd73a7c5cd7f9560dea263603e83b0f2b623ff
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-04-25 15:05:54 +0200

    build: mange env variable for service-worker

--- drama-queen/src/queen-service-worker.js
+++ drama-queen/src/queen-service-worker.js
@@ -1,16 +1,17 @@
 /* eslint-disable no-restricted-globals */
-self._QUEEN_URL = import.meta.env.VITE_QUEEN_URL;
+importScripts(`swEnv.js`)
+self._QUEEN_URL = self.__VITE_ENVS.VITE_QUEEN_URL
 
-importScripts(`${self._QUEEN_URL}/queen-service-worker.js`);
+importScripts(`${self._QUEEN_URL}/queen-service-worker.js`)
 
 const getDramaQueenUrlRegex = (url) => {
   return url
-    .replace("http", "^http")
-    .concat("/(.*)((.js)|(.png)|(.svg)|(.css))");
-};
-const dramaQueenCacheName = "drama-queen-cache";
+    .replace('http', '^http')
+    .concat('/(.*)((.js)|(.png)|(.svg)|(.css))')
+}
+const dramaQueenCacheName = 'drama-queen-cache'
 
-console.log("Loading Drama Queen SW into another SW");
+console.log('Loading Drama Queen SW into another SW')
 
 registerRoute(
   new RegExp(getDramaQueenUrlRegex(self._DRAMAQUEEN_URL)),
@@ -22,22 +23,22 @@ registerRoute(
       }),
     ],
   })
-);
+)
 
 const dramaPrecacheController = async () => {
-  const cache = await caches.open(dramaQueenCacheName);
+  const cache = await caches.open(dramaQueenCacheName)
   const urlsToPrecache = self.__WB_MANIFEST.reduce(
     (_, { url }) => [..._, `${self._DRAMAQUEEN_URL}/${url}`],
     []
-  );
-  await cache.addAll(urlsToPrecache);
-};
+  )
+  await cache.addAll(urlsToPrecache)
+}
 
-self.addEventListener("install", (event) => {
-  console.log("Drama Queen  sw : installing configuration..");
-  event.waitUntil(dramaPrecacheController());
-});
+self.addEventListener('install', (event) => {
+  console.log('Drama Queen  sw : installing configuration..')
+  event.waitUntil(dramaPrecacheController())
+})
 
-self.addEventListener("activate", (event) => {
-  console.log("Drama Queen sw : activating ...");
-});
+self.addEventListener('activate', (event) => {
+  console.log('Drama Queen sw : activating ...')
+})
