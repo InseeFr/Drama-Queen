83774f639be4ac1d5dec51d9c7d03911ac3313a1
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-04-25 15:00:30 +0200

    build: manage env variable for mfe (with vite-envs)

---
+++ drama-queen/build/remote-env.cjs
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const path = require('path')
+
+const reset = '\x1b[0m'
+const log = {
+  green: (text) => console.log('\x1b[32m' + text + reset),
+  blue: (text) => console.log('\x1b[34m' + text + reset),
+  red: (text) => console.log('\x1b[31m' + text + reset),
+}
+
+log.blue(`MFE manage env for remote (micro-frontend)`)
+
+const nameOfMfeEnv = 'mfeEnv.js'
+const entryScriptPath = path.resolve(
+  `${process.cwd()}/dist/assets/${process.argv[2]}`
+)
+const swEnvScriptPath = path.resolve(`${process.cwd()}/dist/swEnv.js`)
+const mfeEnvScriptPath = path.resolve(
+  `${process.cwd()}/dist/assets/${nameOfMfeEnv}`
+)
+
+const isSwEnvScriptExists = fs.existsSync(swEnvScriptPath)
+const isEntryScriptExists = fs.existsSync(entryScriptPath)
+
+if (isSwEnvScriptExists && isEntryScriptExists) {
+  fs.copyFileSync(swEnvScriptPath, mfeEnvScriptPath)
+  const entryContent = fs.readFileSync(entryScriptPath).toString('utf-8')
+  const newEntryContent = `import './${nameOfMfeEnv}';${entryContent}`
+  fs.writeFileSync(entryScriptPath, Buffer.from(newEntryContent, 'utf8'))
+  log.green(`✓ Added env file inside ${entryScriptPath}`)
+} else {
+  log.red(
+    `x Nothing to do. Are you using vite-envs and federation plugin in your configuration vite.config.ts ?`
+  )
+}
