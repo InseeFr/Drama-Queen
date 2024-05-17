97b7f9cba0d2e8a334f144ab394facf1d9ff583d
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-05-17 17:23:40 +0200

    fix: remove double encoding on visualize url
    working for drama-queen with axios, but not working for queen-v1

---
+++ drama-queen/src/ui/pages/visualize/getSearchParams.ts
@@ -0,0 +1,17 @@
+import type { FormValues } from './VisualizeForm'
+
+export function getSearchParams(data: FormValues) {
+  // get encoded form values
+  const stringParams = {
+    questionnaire: data.questionnaire,
+    data: data.data ? data.data : '',
+    nomenclature: data.nomenclature ? JSON.stringify(data.nomenclature) : '',
+    readonly: data.readonly.toString(),
+  }
+  // keep only form values that are filled
+  const searchParams = Object.fromEntries(
+    Object.entries(stringParams).filter(([, value]) => value !== '')
+  )
+
+  return searchParams
+}
