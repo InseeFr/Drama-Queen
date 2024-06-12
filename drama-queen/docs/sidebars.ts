cc8da25a734d7930fbe8be9c23bf5e6b8a4477db
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-06-12 15:59:02 +0200

    docs : initialize content

--- drama-queen/docs/sidebars.ts
+++ drama-queen/docs/sidebars.ts
@@ -18,9 +18,78 @@ const sidebars: SidebarsConfig = {
       label: 'Composants',
       link: {
         type: 'generated-index',
+        description:
+          'Les composants proposés par la filière (et qui peuvent être décrits grâce aux outils Pogues, Eno et Lunatic)',
       },
       collapsed: false,
-      items: [],
+      items: [
+        {
+          type: 'category',
+          label: 'Le balisage',
+          link: {
+            description:
+              "Ces composants offrent des indications visuelles pour aider l'utilisateur à s'orienter dans le formulaire",
+            type: 'generated-index',
+          },
+          collapsed: true,
+          items: [
+            'components/decorations/sequence',
+            'components/decorations/declaration',
+            'components/decorations/global-index',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Les champs de saisie et leur affichage',
+          link: {
+            description:
+              'Cette partie décrit les différents composants proposés dans Stromae DSFR pour saisir des informations (et qui peuvent être décrits grâce aux outils Pogues, Eno et Lunatic',
+            type: 'generated-index',
+          },
+          collapsed: true,
+          items: [
+            'components/fields/input',
+            'components/fields/inputNumber',
+            'components/fields/date',
+            'components/fields/duration',
+            'components/fields/dropdown',
+            'components/fields/suggester',
+            'components/fields/radio',
+            'components/fields/checkboxGroup',
+            'components/fields/table',
+          ],
+        },
+        {
+          type: 'category',
+          label: 'Les agrégateurs',
+          link: {
+            description:
+              "Quelques composants permettant d'agréger des informations complexes (et qui peuvent être décrits grâce aux outils Pogues, Eno et Lunatic)",
+            type: 'generated-index',
+          },
+          collapsed: true,
+          items: [
+            'components/aggregators/pairwise',
+            'components/aggregators/roundabout',
+            'components/aggregators/rosterForLoop',
+          ],
+        },
+      ],
+    },
+    'pagination',
+    {
+      type: 'category',
+      label: 'Fonctionnalités transverses',
+      link: {
+        description: "Fonctionnalités transversales de l'application",
+        type: 'generated-index',
+      },
+      collapsed: true,
+      items: [
+        'transversal/authentification',
+        'transversal/routing',
+        'transversal/orchestratorRouting',
+      ],
     },
   ],
 }
