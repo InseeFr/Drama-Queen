1411efb099f40f62edfaba258407ec72a8b6da50
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-06-14 17:19:38 +0200

    docs : remove edit link

--- drama-queen/docs/docusaurus.config.ts
+++ drama-queen/docs/docusaurus.config.ts
@@ -44,10 +44,6 @@ const config: Config = {
         docs: {
           sidebarPath: './sidebars.ts',
           routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/InseeFr/Drama-Queen/tree/2.1/drama-queen/docs',
         },
         blog: false,
         theme: {
