4a291313ad956493d710783aad5e95a106cc8427
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-07-24 17:38:38 +0200

    chore: run prettier

--- drama-queen/src/unsubscribe_old_sw.ts
+++ drama-queen/src/unsubscribe_old_sw.ts
@@ -1,41 +1,44 @@
-import { DYNAMIC_PUBLIC_URL } from "core";
+import { DYNAMIC_PUBLIC_URL } from 'core'
 
 // Unregister old service workers for standalone
-export const unsubscribeOldSW = () =>
- {if ('serviceWorker' in navigator) {
+export const unsubscribeOldSW = () => {
+  if ('serviceWorker' in navigator) {
+    const isSoloQueen = DYNAMIC_PUBLIC_URL === window.location.origin
 
-	const isSoloQueen = DYNAMIC_PUBLIC_URL === window.location.origin;
-
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    registrations.forEach((registration) => {
-      // Unregister old service worker
-      if (registration.active && registration.active.scriptURL.includes('sw.js')) {
-        registration.unregister().then((success) => {
-          if (success) {
-            console.log('Old service worker unregistered successfully.');
-						// clear cache only if we are in standalone
-						if (isSoloQueen) {
-							clearAllCaches()
-						}
-          } else {
-            console.log('Failed to unregister old service worker.');
-          }
-        });
-      }
-    });
-  });
-}}
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+      registrations.forEach((registration) => {
+        // Unregister old service worker
+        if (
+          registration.active &&
+          registration.active.scriptURL.includes('sw.js')
+        ) {
+          registration.unregister().then((success) => {
+            if (success) {
+              console.log('Old service worker unregistered successfully.')
+              // clear cache only if we are in standalone
+              if (isSoloQueen) {
+                clearAllCaches()
+              }
+            } else {
+              console.log('Failed to unregister old service worker.')
+            }
+          })
+        }
+      })
+    })
+  }
+}
 
 const clearAllCaches = async () => {
   // Get the names of Workbox's default caches
-  const cacheNames = await caches.keys();
+  const cacheNames = await caches.keys()
 
   // Iterate through all cache names and delete them
   await Promise.all(
     cacheNames.map((cacheName) => {
-      return caches.delete(cacheName);
+      return caches.delete(cacheName)
     })
-  );
+  )
 
-  console.log('All caches cleared.');
-};
+  console.log('All caches cleared.')
+}
