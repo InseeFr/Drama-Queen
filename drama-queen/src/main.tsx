f4aab18f99baabd283fe9a159fd16152b4c67d56
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-10-16 10:35:43 +0200

    feat(#242): inject externalResources script at start

--- drama-queen/src/main.tsx
+++ drama-queen/src/main.tsx
@@ -1,10 +1,13 @@
-import('./bootstrap').then(({ mount }) => {
+import('./bootstrap').then(({ mount, mountExternalResources }) => {
   const localRoot = document.getElementById('drama-queen')
 
   mount({
     mountPoint: localRoot!,
     routingStrategy: 'browser',
   })
+
+  if (import.meta.env.VITE_EXTERNAL_RESOURCES_URL)
+    mountExternalResources(import.meta.env.VITE_EXTERNAL_RESOURCES_URL)
 })
 
 export {}
