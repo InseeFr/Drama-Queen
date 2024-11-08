5dd980d0fa1a8305711d4b2fc202f3b3b7d97e35
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2024-11-08 14:53:42 +0100

    fix: mount external resources (#266)
    Entry point for Pearl-Jam is only the `mount` function exported by boostrap.tsx

--- drama-queen/src/bootstrap.tsx
+++ drama-queen/src/bootstrap.tsx
@@ -5,12 +5,20 @@ import { RouterProvider } from 'react-router-dom'
 import { unsubscribeOldSW } from 'unsubscribe_old_sw'
 import { CoreProvider } from 'createCore'
 import { createRouter, type RoutingStrategy } from 'ui/routing/createRouter'
+import { EXTERNAL_RESOURCES_URL } from 'core/constants'
 
 const CenteredSpinner = () => (
   <Stack alignItems="center" justifyContent="center" height="100vh">
     <CircularProgress size={'5em'} />
   </Stack>
 )
+
+const mountExternalResources = (externalResourcesUrl: string) => {
+  console.log('Mount External resources')
+  const script = document.createElement('script')
+  script.src = `${externalResourcesUrl}/entry.js`
+  document.body.appendChild(script)
+}
 
 const mount = ({
   mountPoint,
@@ -26,6 +34,8 @@ const mount = ({
   // unsubscribe to old SW
   unsubscribeOldSW()
 
+  if (EXTERNAL_RESOURCES_URL) mountExternalResources(EXTERNAL_RESOURCES_URL)
+
   const router = createRouter({ strategy: routingStrategy, initialPathname })
   const root = createRoot(mountPoint)
   root.render(
@@ -37,11 +47,4 @@ const mount = ({
   return () => queueMicrotask(() => root.unmount())
 }
 
-const mountExternalResources = (externalResourcesUrl: string) => {
-  console.log('Mount External resources')
-  const script = document.createElement('script')
-  script.src = `${externalResourcesUrl}/entry.js`
-  document.body.appendChild(script)
-}
-
-export { mount, mountExternalResources }
+export { mount }
