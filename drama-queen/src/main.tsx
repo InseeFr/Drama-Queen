44eed51887c71aa683656443789016b1143b2842
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-11-08 14:31:20 +0100

    fix: mount external resources
    Entry point for Pearl-Jam is only the `mount` function exported by boostrap.tsx

--- drama-queen/src/main.tsx
+++ drama-queen/src/main.tsx
@@ -1,14 +1,10 @@
-import { EXTERNAL_RESOURCES_URL } from 'core/constants'
-
-import('./bootstrap').then(({ mount, mountExternalResources }) => {
+import('./bootstrap').then(({ mount }) => {
   const localRoot = document.getElementById('drama-queen')
 
   mount({
     mountPoint: localRoot!,
     routingStrategy: 'browser',
   })
-
-  if (EXTERNAL_RESOURCES_URL) mountExternalResources(EXTERNAL_RESOURCES_URL)
 })
 
 export {}
