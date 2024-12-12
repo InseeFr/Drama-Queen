4ac54e8c7919f7f780b02d8bd8f542b33f607f31
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-12-12 15:32:31 +0100

    test: add tests on fetcher & axios errors handling

---
+++ src/core/tools/fetchUrl.test.ts
@@ -0,0 +1,35 @@
+import { vi, describe, beforeEach, it, expect } from 'vitest'
+import axios from 'axios'
+import { fetchUrl } from './fetchUrl'
+
+vi.mock('axios')
+
+describe('fetchUrl', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should fetch data from the provided URL and return the response data', async () => {
+    const mockUrl = 'https://example.com/api/data'
+    const mockResponseData = { key: 'value' }
+
+    vi.mocked(axios.get).mockResolvedValueOnce({ data: mockResponseData })
+
+    const result = await fetchUrl<{ key: string }>({ url: mockUrl })
+
+    expect(axios.get).toHaveBeenCalledWith(decodeURIComponent(mockUrl))
+
+    expect(result).toEqual(mockResponseData)
+  })
+
+  it('should throw an error if the request fails', async () => {
+    const mockUrl = 'https://example.com/api/error'
+    const mockError = new Error('Request failed')
+
+    vi.mocked(axios.get).mockRejectedValueOnce(mockError)
+
+    await expect(fetchUrl({ url: mockUrl })).rejects.toThrow(mockError)
+
+    expect(axios.get).toHaveBeenCalledWith(decodeURIComponent(mockUrl))
+  })
+})
