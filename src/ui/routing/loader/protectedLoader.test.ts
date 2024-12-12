1c0760e590c73615c45d88a14b101938c83a4538
Quentin Ruhier <quentin.ruhier@insee.fr>
2024-12-12 15:29:36 +0100

    test: add tests on loaders

---
+++ src/ui/routing/loader/protectedLoader.test.ts
@@ -0,0 +1,28 @@
+import { prCore } from 'createCore'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { protectedRouteLoader } from './protectedLoader'
+
+vi.mock('createCore', () => ({
+  prCore: {
+    functions: {
+      userAuthentication: {
+        loginIfNotLoggedIn: vi.fn(),
+      },
+    },
+  },
+}))
+
+describe('protectedRouteLoader', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should call collectSurvey.loader with the correct parameters', async () => {
+    const mockLoader = vi.fn()
+    ;(await prCore).functions.userAuthentication.loginIfNotLoggedIn = mockLoader
+
+    await protectedRouteLoader()
+
+    expect(mockLoader).toHaveBeenCalledWith()
+  })
+})
---
+++ src/ui/routing/loader/visualizeLoader.test.ts
@@ -0,0 +1,32 @@
+import { prCore } from 'createCore'
+import type { LoaderFunctionArgs } from 'react-router-dom'
+import { assert } from 'tsafe'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { visualizeLoader } from './visualizeLoader'
+
+vi.mock('createCore', () => ({
+  prCore: {
+    functions: {
+      visualizeSurvey: {
+        loader: vi.fn(),
+      },
+    },
+  },
+}))
+
+describe('protectedRouteLoader', () => {
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
+
+  it('should call collectSurvey.loader with the correct parameters', async () => {
+    const mockLoader = vi.fn()
+    ;(await prCore).functions.visualizeSurvey.loader = mockLoader
+
+    await visualizeLoader({ request: { url: 'url' } } as LoaderFunctionArgs)
+
+    expect(mockLoader).toHaveBeenCalledWith({
+      requestUrl: 'url',
+    })
+  })
+})
