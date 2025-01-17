2c28c3128d28e66f70c73a156edb29b64df083b7
Quentin Ruhier <quentin.ruhier@insee.fr>
2025-01-17 13:03:43 +0100

    test: make shared wrapper for tests

---
+++ src/tests/TestWrapper.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from '@mui/material/styles'
+
+import type { ReactNode } from 'react'
+
+import { theme } from '@/ui/style/theme'
+
+export const TestWrapper = ({ children }: { children: ReactNode }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
