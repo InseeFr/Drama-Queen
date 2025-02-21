3a201254b6ba8ef18a9611483190605736d28aca
Quentin Ruhier <quentin.ruhier@insee.fr>
2025-02-21 08:45:40 +0100

    test: add tests on synchronize events

---
+++ src/core/usecases/synchronizeData/evt.test.ts
@@ -0,0 +1,58 @@
+import { Evt } from 'evt'
+import { afterEach } from 'node:test'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+
+import { createEvt } from './evt'
+
+describe('createEvt', () => {
+  let evtActionMock: Evt<any>
+
+  beforeEach(() => {
+    evtActionMock = Evt.create<any>()
+  })
+
+  afterEach(() => {
+    vi.resetAllMocks()
+  })
+
+  it('should post "redirect" action when relevant events occur', () => {
+    const evt = createEvt({ evtAction: evtActionMock } as any)
+    const spy = vi.spyOn(evt, 'post')
+
+    // simulate an action that should lead to a redirect
+    evtActionMock.post({
+      usecaseName: 'synchronizeData',
+      actionName: 'uploadError',
+    })
+    evtActionMock.post({
+      usecaseName: 'synchronizeData',
+      actionName: 'downloadCompleted',
+    })
+    evtActionMock.post({
+      usecaseName: 'synchronizeData',
+      actionName: 'downloadFailed',
+    })
+
+    // Verify that post() was called 3 times
+    expect(spy).toHaveBeenCalledTimes(3)
+    expect(spy).toHaveBeenCalledWith({ action: 'redirect' })
+  })
+
+  it('should not post "redirect" action for unrelated events', () => {
+    const evt = createEvt({ evtAction: evtActionMock } as any)
+    const spy = vi.spyOn(evt, 'post')
+
+    // simulate an action that should not lead to a redirect
+    evtActionMock.post({
+      usecaseName: 'otherUsecase',
+      actionName: 'uploadError',
+    })
+    evtActionMock.post({
+      usecaseName: 'synchronizeData',
+      actionName: 'otherAction',
+    })
+
+    // Ensure post() was never called
+    expect(spy).not.toHaveBeenCalled()
+  })
+})
