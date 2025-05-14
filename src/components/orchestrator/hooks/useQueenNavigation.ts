ea084975c456e7ff1118d41a282e4d1a616849f9
Chloe Renaud <chloe.renaud@insee.fr>
2025-05-14 10:35:09 +0200

    fix: last question data loss on definitive quit

--- src/components/orchestrator/hooks/useQueenNavigation.ts
+++ src/components/orchestrator/hooks/useQueenNavigation.ts
@@ -28,16 +28,22 @@ export function useQueenNavigation({
   }
 
   const orchestratorOnDefinitiveQuit = (currentPage: PageTag) => {
-    updateSurveyUnit(getChangedData(true) as SurveyUnitData, { currentPage })
+    let surveyUnit = updateSurveyUnit(getChangedData(true) as SurveyUnitData, {
+      currentPage,
+    })
 
-    // Force the state to COMPLETED only for sending the event. Completed state should be defined by an algorithm.
-    updateSurveyUnit({}, { currentPage, forcedState: 'COMPLETED' })
+    // Force the state to COMPLETED only for sending the event.
+    // Completed state should be defined by an algorithm.
+    surveyUnit = updateSurveyUnit(surveyUnit.data, {
+      currentPage,
+      forcedState: 'COMPLETED',
+    })
 
     // Force the state to VALIDATED.
-    const surveyUnit = updateSurveyUnit(
-      {},
-      { currentPage, forcedState: 'VALIDATED' },
-    )
+    surveyUnit = updateSurveyUnit(surveyUnit.data, {
+      currentPage,
+      forcedState: 'VALIDATED',
+    })
 
     return onDefinitiveQuit(surveyUnit)
   }
