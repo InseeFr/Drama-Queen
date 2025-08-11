34338d0f3b82de7c7996a7d5f9fe0080e7f19a0f
Mailine Nguyen <nguyen.mailine@gmail.com>
2025-08-11 08:24:46 +0200

    chore(deps): switch hotkeys library to react-hotkeys-hook

--- src/components/ui/ShortCut.tsx
+++ src/components/ui/ShortCut.tsx
@@ -1,31 +1,22 @@
-import ReactHotkeys from 'react-hot-keys'
+import { useHotkeys } from 'react-hotkeys-hook'
 
 type ShortCutProps = {
   shortCutKey: string
   onClickMethod: () => void
 }
 
-/** Allow to add an action that will be triggered through a hotkey. */
 export function ShortCut({
   shortCutKey,
   onClickMethod,
 }: Readonly<ShortCutProps>) {
-  function onClickShortCut(onClickMethod: () => void) {
-    return (_keyName: string, event: KeyboardEvent) => {
+  useHotkeys(
+    shortCutKey,
+    (event) => {
       onClickMethod()
       event.preventDefault()
-    }
-  }
-
-  const handleShortCut = onClickShortCut(onClickMethod)
-
-  // filter ables to overload native navigation
-  return (
-    <ReactHotkeys
-      key={shortCutKey}
-      keyName={shortCutKey}
-      onKeyDown={handleShortCut}
-      filter={() => true}
-    />
+    },
+    { preventDefault: true, enableOnFormTags: ['input', 'select', 'textarea'] },
   )
+
+  return null
 }
