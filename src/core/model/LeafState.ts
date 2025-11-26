8813ed95033b3be1134c4f08e247faece9659fb7
Jonathan <395137+Grafikart@users.noreply.github.com>
2025-11-26 10:35:28 +0100

    feat: add cell data or articulation table (not anonymous) (#378)

--- src/core/model/LeafState.ts
+++ src/core/model/LeafState.ts
@@ -3,4 +3,5 @@ export type LeafStateState = 'NOT_INIT' | 'INIT' | 'COMPLETED' | null
 export type LeafState = {
   state: LeafStateState
   date: number
+  cells?: { label: string; value: string }[]
 }
