75bf15223fc400d5f75d5f9fed60e4adfd92e859
Laurent Caouissin <caouissin.laurent@gmail.com>
2026-01-14 10:35:48 +0100

    ref: remove useless import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { federation } from '@module-federation/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 import { viteEnvs } from 'vite-envs'
 import { VitePWA } from 'vite-plugin-pwa'
 import tsconfigPaths from 'vite-tsconfig-paths'
