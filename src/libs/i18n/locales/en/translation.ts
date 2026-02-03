13dfd5a2712ad5c7ccbc40e814788f6ee622c7ba
Quentin Ruhier <quentin.ruhier@insee.fr>
2026-02-03 17:17:35 +0100

    chore: create new translation files

---
+++ src/libs/i18n/locales/en/translation.ts
@@ -0,0 +1,13 @@
+import envValues from './envValues.json'
+import error from './error.json'
+import navigation from './navigation.json'
+import synchronize from './synchronize.json'
+import visualize from './visualize.json'
+
+export const translation = {
+  envValues,
+  error,
+  navigation,
+  synchronize,
+  visualize,
+}
---
+++ src/libs/i18n/resources/fr.ts
@@ -0,0 +1,107 @@
+import { LUNATIC_MODEL_VERSION_BREAKING } from '@/core/constants'
+
+import type { Translations } from '../types'
+
+export const translations: Translations<'fr'> = {
+  errorMessage: {
+    '400': 'Requête invalide.',
+    '401':
+      "Vous n'êtes pas connecté. Veuillez vous connecter pour accéder à cette ressource.",
+    '403': "Vous n'êtes pas autorisé à accéder aux données demandées",
+    '404': 'Ressource(s) non trouvée(s).',
+    '500': 'Erreur interne du serveur.',
+    '502': 'Passerelle incorrecte.',
+    '503': 'Service indisponible.',
+    '504': "Délai d'attente de la passerelle expiré.",
+    error: 'Erreur',
+    errorOccured: 'Une erreur est survenue',
+    shortUnknownError: 'Erreur inconnue',
+    longUnknownError:
+      "Une erreur inconnue s'est produite, veuillez contacter l'assistance ou réessayer plus tard.",
+    interrogationNotRetrievable:
+      "Une erreur est survenue lors de la récupération de l'interrogation.",
+    interrogationNotFound: ({ interrogationId }) =>
+      `L'interrogation ${interrogationId} est introuvable.`,
+    lunaticModelVersionNotFound:
+      'Le questionnaire ne comporte pas de champ lunaticModelVersion',
+    questionnaireNotCompatible: `Le questionnaire est incompatible. La version du 'lunaticModelVersion' doit être supérieure à  ${LUNATIC_MODEL_VERSION_BREAKING}`,
+    interrogationQuestionnaireNotFound: ({ interrogationId }) =>
+      `Impossible de récupérer le questionnaire de l'unité d'enquête ${interrogationId}.`,
+    questionnaireNotFound: ({ questionnaireId }) =>
+      `Impossible de récupérer le questionnaire ${questionnaireId}.`,
+    wrongQuestionnaire: ({ interrogationId, questionnaireId }) =>
+      `L'interrogation ${interrogationId} n'est pas associée au questionnaire ${questionnaireId}.`,
+    interrogationUnauthorized:
+      "Vous n'êtes pas autorisé à accéder aux données de cette interrogation.",
+    externalResourcesLoadedError:
+      'Impossible de charger les ressources externes.',
+  },
+  modalMessage: {
+    reviewQuitTitle: 'Sortie du questionnaire',
+    reviewQuitContent:
+      "Si vous souhaitez sortir du questionnaire, veuillez fermer l'onglet actuel.",
+    temporaryQuitTitle: 'Arrêt provisoire',
+    temporaryQuitContent: 'Vous allez sortir du questionnaire',
+    temporaryQuitValidate: 'Valider',
+    definitiveQuitTitle: 'Arrêt définitif',
+    definitiveQuitContent:
+      'Confirmez-vous l’arrêt définitif du questionnaire ?',
+    definitiveQuitValidate: "Valider l'arrêt définitif",
+    welcomeModalTitle: 'Bienvenue',
+    welcomeModalContent:
+      'Vous avez déjà commencé à renseigner le questionnaire. Souhaitez-vous reprendre là où vous en étiez ou revenir à la première page ?',
+    welcomeModalFirstPage: 'Revenir à la première page',
+    welcomeModalGoBack: "Reprendre là où j'en étais",
+    cancel: 'Annuler',
+  },
+  navigationMessage: {
+    goToSequence: 'Aller vers la séquence',
+    goToSubSequence: 'Aller vers la sous-séquence',
+    goTo: 'Aller vers ...',
+    continueHelper: 'appuyer sur',
+    previousHelper: 'PREC.',
+    nextHelper: 'SUIV.',
+    backToQuestionnaireStart: 'Retour au début du questionnaire',
+    temporaryQuestionnaireStop: "Arrêt provisoire de l'interview",
+    definitiveQuestionnaireStop:
+      "Arrêt définitif de l'interview (refus, impossibilité de continuer, ...)",
+    questionnaireStopNature: "Quelle est la nature de l'arrêt ?",
+    back: 'Retour',
+    surveyButton: 'Enquête',
+    stopButton: 'Arrêt',
+    pageNumber: 'n° page',
+    validateAndQuit: 'Valider et quitter',
+    quit: 'Quitter',
+    fastForward: "Suite de l'entretien",
+    continue: 'Continuer',
+    dontKnowButtonLabel: 'Ne sait pas',
+    shortCutNextLabel: 'alt + ENTRÉE',
+  },
+  synchronizeMessage: {
+    synchronizationInProgress: 'Synchronisation en cours',
+    downloadingData: 'Téléchargement des données...',
+    interrogationsProgress: 'Unités enquêtées',
+    paradataProgress: 'Paradonnées',
+    questionnairesProgress: 'Questionnaires',
+    nomenclaturesProgress: 'Nomenclatures',
+    externalResourcesProgress: 'Ressources externes',
+    uploadingData: 'Envoi des données...',
+    takingControl: 'Reprise du questionnaire...',
+  },
+  visualizeMessage: {
+    visualizePage: 'Page de visualisation de questionnaire',
+    inputSurveyLabel: 'Questionnaire',
+    inputSurveyHelper:
+      "L'url d'un json de questionnaire au format Lunatic-model",
+    inputDataLabel: 'Données',
+    inputDataHelper: "L'url d'un json de données (de réponse)",
+    inputNomenclatureLabel: 'Dictionnaire de nomenclatures',
+    inputNomenclatureHelper:
+      "Dictionnaire avec en clé le nom de la nomenclature et en valeur l'url",
+    visualizeButtonLabel: 'Visualiser',
+    readonlyLabel: 'Lecture seule',
+  },
+  envValuesMessage: {
+    envVariables: "Les variables d'environnements",
+  },
+}
