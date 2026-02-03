99df34cd3d1bfc0e152d22a0fa4831c4eb338bff
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2026-02-03 11:44:51 +0100

    chore: remove unused file

--- router.config.ts
+++
@@ -1,9 +0,0 @@
-import type { RouterConfig } from '@tanstack/router-cli'
-
-const config: RouterConfig = {
-  routesDirectory: 'src/routes',
-  routeFileIgnorePrefix: '-',
-  routeFileIgnorePattern: /\/_layout\/|\/\$[^/]+\//,
-}
-
-export default config
