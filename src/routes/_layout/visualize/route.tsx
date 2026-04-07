024de8becf200c18d97b2f1ff9c86a54e52af976
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-04-07 16:13:07 +0200

    fix: visualize route

--- src/routes/_layout/visualize/route.tsx
+++ src/routes/_layout/visualize/route.tsx
@@ -1,21 +1,23 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 import { visualizeLoader } from '@/core/loader'
+import type { FormValues } from '@/pages/visualize/models'
 
 import { Visualize } from './index'
 
-export type VisualizeSearch = {
-  questionnaire?: string
-  data?: string
-  nomenclature?: string
-  readonly?: boolean
-}
+export type VisualizeSearch = FormValues
 
 const parseStringSearch = (value: unknown) =>
-  typeof value === 'string' ? value : undefined
+  typeof value === 'string' ? value : ''
 
 const parseBooleanSearch = (value: unknown) =>
-  typeof value === 'boolean' ? value : undefined
+  typeof value === 'boolean' ? value : false
+
+const parseObjectSearch = (value: unknown) => {
+  return typeof value === 'object'
+    ? (value as Record<string, string>)
+    : undefined
+}
 
 export const Route = createFileRoute('/_layout/visualize')({
   component: Visualize,
@@ -23,7 +25,7 @@ export const Route = createFileRoute('/_layout/visualize')({
   validateSearch: (search: Record<string, unknown>): VisualizeSearch => ({
     questionnaire: parseStringSearch(search.questionnaire),
     data: parseStringSearch(search.data),
-    nomenclature: parseStringSearch(search.nomenclature),
+    nomenclature: parseObjectSearch(search.nomenclature),
     readonly: parseBooleanSearch(search.readonly),
   }),
 })
