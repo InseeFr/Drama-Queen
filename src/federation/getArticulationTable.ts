42ae0f7c6ebbc752d89c86d14694680d4512804a
Laurent Caouissin <laurent.caouissin@insee.fr>
2026-09-21 17:36:32 +0200

    fix: try getArticulationTable fix with fallback

--- src/federation/getArticulationTable.ts
+++ src/federation/getArticulationTable.ts
@@ -2,7 +2,7 @@ import { type LunaticSource, getArticulationState } from '@inseefr/lunatic'
 
 import { type ReactNode } from 'react'
 
-import type { LeafStateState } from '@/core/model'
+import type { LeafStateState, LocalInterrogation } from '@/core/model'
 import { prCore } from '@/createCore'
 
 export type TableData = {
@@ -35,31 +35,15 @@ export async function getArticulationTable(
     return null
   }
 
-  // Use leafState
   if (!interrogation.data) {
-    if (!interrogation?.stateData?.leafStates) {
-      return null
-    }
-    return {
-      dates: interrogation?.stateData?.leafStates.map((s) => s.date),
-      headers:
-        interrogation?.stateData.leafStates[0]?.cells?.map((c) => c.label) ??
-        [],
-      rows: interrogation?.stateData.leafStates.map((leafState) => ({
-        cells: leafState.cells ?? [],
-        url: null,
-        page: null,
-        label: progressLabel(leafProgress(leafState.state)),
-        // progress is intentionally not part of the public type
-        progress: leafProgress(leafState.state),
-      })),
-    }
+    return buildAnonymousTableWithLeafStates(interrogation)
   }
 
   // Extract articulation data
   const { items } = getArticulationState(questionnaire, interrogation.data)
   if (items.length === 0) {
-    return null
+    // fallback to leafState => anonymous table
+    return buildAnonymousTableWithLeafStates(interrogation)
   }
 
   // Build the result
@@ -70,6 +54,26 @@ export async function getArticulationTable(
       ...item,
       url: buildUrl(interrogationId, item.page),
       label: progressLabel(item.progress),
+    })),
+  }
+}
+
+const buildAnonymousTableWithLeafStates = (
+  interrogation: LocalInterrogation,
+) => {
+  if (!interrogation?.stateData?.leafStates) {
+    return null
+  }
+  return {
+    dates: interrogation?.stateData?.leafStates.map((s) => s.date),
+    headers: [],
+    rows: interrogation?.stateData.leafStates?.map((leafState) => ({
+      cells: leafState.cells ?? [],
+      url: null,
+      page: null,
+      label: progressLabel(leafProgress(leafState.state)),
+      // progress is intentionally not part of the public type
+      progress: leafProgress(leafState.state),
     })),
   }
 }
@@ -100,7 +104,11 @@ const progressLabel = (n: number) => {
 }
 
 const leafProgress = (leafState: LeafStateState) => {
-  if (leafState === 'NOT_INIT') {
+  if (
+    leafState === 'NOT_INIT' ||
+    leafState === null ||
+    leafState === undefined
+  ) {
     return -1
   }
   if (leafState === 'INIT') {
